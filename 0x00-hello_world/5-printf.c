26b833128a9dc2f217a3de60f9d39e799eed91a1
Maryjanechidinma <maryjanechidinma34@gmail.com>
2022-09-08 17:55:50 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry piont
  *
- * * Description: prints "with proper grammar, but the outcome is a piece of art,"
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
  *
  * Return: Always 0 (Success)
  */
