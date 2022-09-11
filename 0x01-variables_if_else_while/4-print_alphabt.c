aa8af359e6b3d9c90c9ce71276ad55ef6faf5080
Maryjanechidinma <maryjanechidinma34@gmail.com>
2022-09-11 03:15:18 -0700

    a-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 	char letter;
-	
-	for (letter = 'a'; letter <= 'z'; letter++)
-        {
-	        if (letter != 'e' && letter != 'q')
+
+        for (letter = 'a'; letter <= 'z'; letter++)
+        { 
+                if (letter != 'e' && letter != 'q')
 		        putchar(letter);
-	}
-            
+        }
+ 
         putchar('\n');
 
         return (0);
