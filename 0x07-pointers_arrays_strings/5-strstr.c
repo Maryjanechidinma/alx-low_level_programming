ccd85b063ef3304befa089b5f5a8b84b629947e1
Maryjanechidinma <maryjanechidinma34@gmail.com>
2022-09-27 11:27:58 -0700

    101-crackme_password

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,31 +2,31 @@
 
 /**
  * _strstr - locates a substring
- * @heystack: string in which to check for needle
- * @needle: sunstring to find in heystack
+ * @haystack: string in which to check for needle
+ * @needle: substring to find in haystack
  *
- * Return: pointer to beginning of needle in heystack or NULL if no match
+ * Return: pointer to beginning of needle in haystack or NULL if no match
  */
-char *_strstr(char *heystack, char *needle)
+char *_strstr(char *haystack, char *needle)
 {
 	unsigned int i = 0, j = 0;
 
-	while (heystack[i])
+	while (haystack[i])
 	{
-		while (needle[j] && (heystack[i] == needle[0]))
+		while (needle[j] && (haystack[i] == needle[0]))
 		{
-			if (heystack[i + j] == needles[j])
+			if (haystack[i + j] == needle[j])
 				j++;
 			else
 				break;
 		}
 		if (needle[j])
 		{
-			j++;
+			i++;
 			j = 0;
 		}
 		else
-			return (heystack + i);
+			return (haystack + i);
 	}
 	return (0);
 }
