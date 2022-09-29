5c8efa7f337e7a5d89a1ac84bd63ff779521182e
Maryjanechidinma <maryjanechidinma34@gmail.com>
2022-09-28 17:29:13 -0700

    recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - return the value of x raised to the power of y
+ * @x: number to be printed
+ * @y: exponent to be used
+ *
+ * Return: x ^ y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	esle if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
