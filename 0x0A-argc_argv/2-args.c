bcadb6a79b96562485a3848ab1b85281e6d22172
Maryjanechidinma <maryjanechidinma34@gmail.com>
2022-10-01 11:05:04 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,13 +5,13 @@
  * @argc: The nmuber of arguments supplied to the program
  * @argv: An array of pointers to the arguments.
  *
- * Return: Always 0.
+ * Return: always 0.
  */
 int main(int argc, char *argv[])
 {
 	int arg;
 
-	for (arg = 0; arg < argc; agr++)
+	for (arg = 0; arg < argc; arg++)
 	{
 		printf("%s\n", argv[arg]);
 	}
