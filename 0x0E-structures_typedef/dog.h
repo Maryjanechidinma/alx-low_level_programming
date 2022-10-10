579c4c6a8ad6eb8a8b07d24a5c44075e0095a59e
Maryjanechidinma <maryjanechidinma34@gmail.com>
2022-10-10 15:19:24 -0700

    Task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef STRTYP
-#define STRTYP
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - A new type describing a dog.
@@ -24,4 +24,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /* STRTYP*/
+#endif /* DOG_H*/
