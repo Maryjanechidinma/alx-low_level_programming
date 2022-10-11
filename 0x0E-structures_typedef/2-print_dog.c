a02ecd2cc75148f62e824db31ed1924303b534c7
Maryjanechidinma <maryjanechidinma34@gmail.com>
2022-10-11 13:56:19 -0700

    task2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,13 +8,19 @@
 
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d == 0)
+	{
+		return;
+	}
+	else
 	{
 		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
+
 		printf("Age: %f\n", d->age);
+
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
 		else
