b7782a33c42f0ee7076a09b824eebb9e43048e30
Maryjanechidinma <maryjanechidinma34@gmail.com>
2022-10-22 17:15:42 -0700

    personal project

---
+++ 0x02-functions_nested_loops/0-putchar1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+    char ch;
+    printf("Print all uppercase letters followed by a new line\n");
+   for (ch= 'A'; ch<= 'Z'; ch++)
+    putchar(ch);
+    putchar ('\n' ) ;
+    getc;
+}
