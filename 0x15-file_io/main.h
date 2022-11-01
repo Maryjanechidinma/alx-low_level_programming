4ef960211e6d3740a1baaa087cabb2d4ce0a3dfa
Maryjanechidinma <maryjanechidinma34@gmail.com>
2022-10-31 17:42:14 -0700

    task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
 #include <sys/tpyes.h>
 #include <sys/stat.h>
 #include <fcnt1.h>
@@ -10,4 +11,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#end /* MAIN_H */
+#end /*MAIN_H*/
